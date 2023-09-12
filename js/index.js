72b075e27da90ddcd9cd29a9022a403fb6110bf5
ayoubbouazi <83404255+ayoubbouazi@users.noreply.github.com>
2023-09-12 09:35:23 +0200

    END OF THE TEST

--- js/index.js
+++ js/index.js
@@ -1,32 +1,59 @@
+// ACTIVE INICIO
 $(document).ready(function () {
-    // Initially set the "INICIO" link as active
-    $("ul.nav-menu li:first-child a").addClass("active");
+  // Initially set the "INICIO" link as active
+  $("ul.nav-menu li:first-child a").addClass("active");
 
-    // Add click event handler to all menu items
-    $("ul.nav-menu li a").click(function () {
-        // Remove the "active" class from all links
-        $("ul.nav-menu li a").removeClass("active");
+  // Add click event handler to all menu items
+  $("ul.nav-menu li a").click(function () {
+    // Remove the "active" class from all links
+    $("ul.nav-menu li a").removeClass("active");
 
-        // Add the "active" class to the clicked link
-        $(this).addClass("active");
-    });
+    // Add the "active" class to the clicked link
+    $(this).addClass("active");
+  });
 });
 
 
 // MOVE DOWN
-
-$(document).ready(function() {
-  // Función para animar la imagen hacia abajo
-  $(".move-down").click(function() {
-    $(this).animate({ top: "+=30px" }, 500); // Ajusta la distancia de animación según tus necesidades
+$(document).ready(function () {
+  // Function to cheer up the image down
+  $(".move-down").click(function () {
+    $(this).animate({ top: "+=30px" }, 500); // Adjust the animation distance according to your needs
   });
 
   // Función para enviar hacia abajo al hacer clic
-  $(".move-down").on("click", function() {
-    var distancia = 1000; // Cambia esta cantidad a la distancia que desees
+  $(".move-down").on("click", function () {
+    var distancia = 800; // Change this amount at the distance you want
     $("html, body").animate({ scrollTop: distancia }, "slow");
     return false;
   });
 });
 
 
+// MODAL NAVBAR
+$(document).ready(function () {
+  // Capture click on the "Menu" link
+  $("a[id='modal']").click(function () {
+    // Muestra el modal
+    $("#miModal").css("display", "block");
+  });
+
+  // Capture click on the modal closure button
+  $(".close").click(function () {
+    // Cierra el modal
+    $("#miModal").css("display", "none");
+  });
+});
+
+
+$(document).ready(function() {
+  const icon = $('.fix-wsp-icon');
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) { // Adjust the displacement threshold as necessary
+      icon.addClass('show');
+    } else {
+      icon.removeClass('show');
+    }
+  });
+});
